a1876036a710231b0bb739f4de5bff07bea6aa46
Hitesh Patel <hites_297@yahoo.co.in>
2025-12-28 21:05:34 -0500

    server and db health check update

--- src/Portfolio.API/Controllers/HealthController.cs
+++ src/Portfolio.API/Controllers/HealthController.cs
@@ -15,14 +15,19 @@ namespace Portfolio.API.Controllers
             _db = db;
         }
 
-        
-
         [HttpGet]
         [Route("ping")]
         public async Task<IActionResult> Ping(CancellationToken ct)
         {
-            await _db.Database.ExecuteSqlRawAsync("SELECT 1", ct);
             return Ok(new { status = "API is up and running" });
+        }
+
+        [HttpGet]
+        [Route("database")]
+        public async Task<IActionResult> Database(CancellationToken ct)
+        {
+            await _db.Database.ExecuteSqlRawAsync("SELECT 1", ct);
+            return Ok(new { status = "Database is up and running" });
         }
     }
 }
